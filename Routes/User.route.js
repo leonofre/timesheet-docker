3b5a0ae0b9cc98efcc192ef87a2058845e39d8b6
Leonardo Dias Kindle <46493043+leonardo-dias-kindle@users.noreply.github.com>
2020-11-27 00:44:13 -0300

    Moved Create User route to app.js

--- Routes/User.route.js
+++ Routes/User.route.js
@@ -6,9 +6,6 @@ const UserController = require('../Controllers/User.Controller');
 //Get a list of all Users
 router.get('/', UserController.getAllUsers);
 
-//Create a new User
-router.post('/', UserController.createNewUser);
-
 //Get a User by id
 router.get('/:id', UserController.findUserById);
 
